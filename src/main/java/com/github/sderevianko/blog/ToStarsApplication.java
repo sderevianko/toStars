97a1339595deba7b3c8ebfcb016f0f7727627c91
sderevianko <derevianko.serhii@gmail.com>
2020-07-01 01:33:04 +0300

    Initial commit

---
+++ src/main/java/com/github/sderevianko/blog/ToStarsApplication.java
@@ -0,0 +1,13 @@
+package com.github.sderevianko.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToStarsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToStarsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/sderevianko/blog/ToStarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sderevianko.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToStarsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
